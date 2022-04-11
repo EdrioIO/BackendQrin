71c5eee505b056b3d38f86000d24d154c9a720ea
edrio <edriolich@gmail.com>
2022-04-11 20:08:49 +0700

    Add listen port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/multiple', function (req, res) {
     });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port 3000');
 }) // jalanin di port 3000
 
