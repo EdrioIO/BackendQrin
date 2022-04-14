f41bb56a3674a0a2d025f2c0a78e47cf9696a516
edrio <edriolich@gmail.com>
2022-04-14 13:50:21 +0700

    Connect to database

--- database.js
+++ database.js
@@ -1,5 +1,14 @@
-const {Client} = require('pg');
+const { Client } = require('pg')
 
 const client = new Client({
-    
+  user: 'axgmnmooasvcsg',
+  host: 'ec2-52-86-56-90.compute-1.amazonaws.com',
+  database: 'dadbpgie5t1j5t',
+  password: '1254a4ae582a6dd1e5458e3ae19f8561789a45b25ce6bbaf969d765264cbef46',
+  port: 5432,
 })
+
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
