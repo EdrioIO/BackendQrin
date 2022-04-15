dd50b40ae7cbf13cc2660127ac879fb44aa996ef
edrio <edriolich@gmail.com>
2022-04-15 12:13:57 +0700

    Database connected

--- database.js
+++ database.js
@@ -1,39 +1,47 @@
-const { Client } = require('pg')
+require('dotenv').config()
 
-const client = new Client({
-  user: process.env.USER_KEY,
-  host: process.env.HOST_KEY,
-  database: process.env.DATABASE_KEY,
-  password: process.env.PASSWORD_KEY,
-  port: process.env.PORT_KEY,
+// manual 1 by 1
 
-ssl: {
-    rejectUnauthorized: false
-  }
-
-})
-
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-// const { Client } = require('pg');
+// const { Client } = require('pg')
 
 // const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
+//   user: process.env.USER_KEY,
+//   host: process.env.HOST_KEY,
+//   database: process.env.DATABASE_KEY,
+//   password: process.env.PASSWORD_KEY,
+//   port: process.env.PORT_KEY,
+
+// ssl: {
 //     rejectUnauthorized: false
 //   }
-// });
 
-// client.connect();
+// })
 
-// client.query('SELECT * FROM "MsStudent"', (err, res) => {
+// client.connect(function(err) {
 //   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-  
+//   console.log("Connected!");
 // });
+
+
+// one single url
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+const query = 'Select * ' + 'ST001'
+ 
+client.query<JSON>('SELECT * FROM "MsStudent" ', 'where "StudendId" = $ST001', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+  
+});
