b6e0efe61039e0ce6de660fc149e028d660a2365
edrio <edriolich@gmail.com>
2022-04-17 15:58:29 +0700

    produciton env

--- database/knexfile.js
+++ database/knexfile.js
@@ -5,8 +5,8 @@
  */
 module.exports = {
 
-  development: {
-    client: 'pg',
+  production: {
+    client: "pg",
     connection: process.env.DATABASE_URL,
     pool: { //connection
       min: 2,
