fef4714e0982be10203baf66a09c69391408bae9
edrio <edriolich@gmail.com>
2022-04-24 17:48:46 +0700

    tryiing removing development from env options

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,4 +1,4 @@
-const dbEngine = process.env.DB_ENV || 'development';
+const dbEngine = process.env.DB_ENV ;
 const config = require('./knexfile')[dbEngine];
 
 module.exports = require("knex")(config);
