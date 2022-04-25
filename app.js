9be9118e0ed3a8e2e0800defd491b5f4008809bc
edrio <edriolich@gmail.com>
2022-04-25 19:53:34 +0700

    changing port env

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config();
 const server = require('./server/server');
 
-const port = process.env.PORT_KEY || 3000;
+const port = process.env.PORT || 3000;
 
 
-server.listen(port, () => {
+server.listen(process.env, () => {
     console.log(`listening on port ${port}\n`);
 }) // jalanin di port yang di auto assign / 3000
 
