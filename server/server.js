1c2b3858a29a02a6618fce9e1d77770c169037cf
edrio <edriolich@gmail.com>
2022-04-25 19:45:35 +0700

    exclude testing routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const loginRouter = require('../routes/login-routes');
+// const loginRouter = require('../routes/login-routes');
 const userRouter = require('../routes/user');
 
 const server = express();
 server.use(express.json());
 
 
-server.use('/api/staff', loginRouter);
+// server.use('/api/staff', loginRouter);
 server.use('/api/user', userRouter);
 
 // server.use('./api/submit') // tambahin route object
