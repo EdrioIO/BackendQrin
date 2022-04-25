bc9b1b9e2bbbb7baf4ed77536cfb5ba042bcfe04
edrio <edriolich@gmail.com>
2022-04-25 20:03:09 +0700

    rev listening port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = require('./server/server');
 const port = process.env.PORT || 3000;
 
 
-server.listen(process.env, () => {
+server.listen(port, () => {
     console.log(`listening on port ${port}\n`);
 }) // jalanin di port yang di auto assign / 3000
 
