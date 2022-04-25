b47d7cb1e6b5226e26625148d5a81ce5a706a4e7
edrio <edriolich@gmail.com>
2022-04-25 19:48:11 +0700

    adding root route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const server = express();
 server.use(express.json());
 
 
+server.get('/', (req,res)=>{
+    res.json({message : "this is homepage root for testing"})
+})
+
 // server.use('/api/staff', loginRouter);
 server.use('/api/user', userRouter);
 
