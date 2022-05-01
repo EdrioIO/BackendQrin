855521cedf2497aeb15759882fb3731ab864d61a
edrio <edriolich@gmail.com>
2022-05-01 10:39:59 +0700

    select placement revision

--- models/dbHelpers.js
+++ models/dbHelpers.js
@@ -37,17 +37,18 @@ module.exports = {
 
 
 function grabAttendData(student_nim, qr_code) {
-    return db.select('ms_student.student_nim', 'ms_session.qr_code', 'cla.class_coor_x', 'cla.class_coor_y', 'cla.class_coor_z', 'att.presence_in_time', 'att.presence_out_time').from('ms_student')
+    return db('ms_student')
         .join(('ms_attendance'), ms_student.student_id = ms_attendance.student_id)
         .join(('ms_session_header'), ms_session_header.session_header_id = ms_attendance.session_header_id)
         .join(('ms_session'), ms_session_header.session_id = ms_session.session_id)
         .join(('ms_class'), ms_class.class_id = ms_session_header.class_id)
+        .select('ms_student.student_nim', 'ms_session.qr_code', 'cla.class_coor_x', 'cla.class_coor_y', 'cla.class_coor_z', 'att.presence_in_time', 'att.presence_out_time')
         .where({ 'ms_student.student_nim': student_nim, 'ms_session.qr_code': qr_code })
 }
 
 function alterPresenceData(student_nim, attend_type, currentTime) {
     if (attend_type == 'in') {
-        db.select('ms_student.student_nim', 'ms_session.qr_code', 'cla.class_coor_x', 'cla.class_coor_y', 'cla.class_coor_z', 'att.presence_in_time', 'att.presence_out_time').from('ms_student')
+        db('ms_student')
             .join(('ms_attendance'), ms_student.student_id = ms_attendance.student_id)
             .join(('ms_session_header'), ms_session_header.session_header_id = ms_attendance.session_header_id)
             .join(('ms_session'), ms_session_header.session_id = ms_session.session_id)
@@ -57,11 +58,12 @@ function alterPresenceData(student_nim, attend_type, currentTime) {
 
     }
     else {
-        db({ stu: 'ms_student' })
-            .join({ att: 'ms_attendance' }, stu.student_id = att.student_id)
-            .join({ seh: 'ms_session_header' }, seh.session_header_id = att.session_header_id)
-            .join({ ses: 'ms_session' }, seh.session_id = ses.session_id)
-            .where({ 'stu.student_nim': student_nim, 'ses.qr_code': qr_code })
+        db('ms_student')
+            .join(('ms_attendance'), ms_student.student_id = ms_attendance.student_id)
+            .join(('ms_session_header'), ms_session_header.session_header_id = ms_attendance.session_header_id)
+            .join(('ms_session'), ms_session_header.session_id = ms_session.session_id)
+            .join(('ms_class'), ms_class.class_id = ms_session_header.class_id)
+            .where({ 'ms_student.student_nim': student_nim, 'ms_session.qr_code': qr_code })
             .update({ presence_out_time: currentTime })
     }
 }
