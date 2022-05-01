2d26b3ef79e54b60431d8b95a87c8d83aff0cb88
edrio <edriolich@gmail.com>
2022-05-01 16:00:42 +0700

    removing str from date param

--- models/timeHelper.js
+++ models/timeHelper.js
@@ -24,7 +24,7 @@ async function getCurrentTime(){
     // const getColTimeFromDate = date => date.toTimeString().slice(0,8);
     // const ex = await getColTimeFromDate(new Date());
     // return ex;
-    var timeNow = (new Date(str)).toLocaleTimeString(defaultlang, options);
+    var timeNow = new Date().toLocaleTimeString();
     return timeNow;
 }
 
