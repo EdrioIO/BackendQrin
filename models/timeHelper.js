445d537683d898425611fb2c445bdc9f6126e905
edrio <edriolich@gmail.com>
2022-05-03 16:06:32 +0700

    a

--- models/timeHelper.js
+++ models/timeHelper.js
@@ -19,7 +19,7 @@ function convertTZ(date, tzString) {
 }
 
 // usage: Asia/Jakarta is GMT+7
-console.log(convertTZ(new Date(), "Asia/Jakarta").toDateString()) // Tue Apr 20 2012 17:10:30 GMT+0700 (Western Indonesia Time)
+console.log(convertTZ(new Date(), "Asia/Jakarta").getDate()) // Tue Apr 20 2012 17:10:30 GMT+0700 (Western Indonesia Time)
 
 // Resulting value is regular Date() object
 const convertedDate = convertTZ("2012/04/20 10:10:30 +0000", "Asia/Jakarta")
