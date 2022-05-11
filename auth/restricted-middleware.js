abb4b21034d244240802bfb2150db25d21a8c26e
edrio <edriolich@gmail.com>
2022-05-11 20:13:11 +0700

    cabut session

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,9 +1,9 @@
 module.exports = (req,res,next) =>{
     console.log('req.session : ', req.session)
-    if(req.session && req.session.user){// if there is user then next
+    // if(req.session && req.session.user){// if there is user then next
         next();// continue to next route on server.js
-    }
-    else{
-        res.status(401).json({error : true, message : 'No cookie session when operating function'});
-    }
+//     }
+//     else{
+//         res.status(401).json({error : true, message : 'No cookie session when operating function'});
+//     }
 }
