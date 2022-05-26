fefb8eecc62d9c67040fbb55efa836d35a7c775c
edrio <edriolich@gmail.com>
2022-05-26 15:05:28 +0700

    tshot

--- models/dbHelpers.js
+++ models/dbHelpers.js
@@ -283,7 +283,6 @@ function grabTeacherCourseNot(teacher_id) {
 
     return db('ms_course')
         .join('ms_course_teached','ms_course.course_id','ms_course_teached.course_id')
-        .where({teacher_id})
         .whereNotExists(db.select('*').from('ms_course_teached')
             .where({ teacher_id })
             .whereRaw('ms_course.course_id = ms_course_teached.course_id'))
